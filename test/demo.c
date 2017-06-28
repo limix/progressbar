af4f9687bdaf4ebbc5b720a7be474331469eb949
Danilo Horta <danilo.horta@gmail.com>
2017-06-28 07:26:16 +0100

    windows

--- test/demo.c
+++ test/demo.c
@@ -1,5 +1,9 @@
 #include "progressbar.h"
-#include <unistd.h>
+#ifdef _WIN32
+# include <windows.h>
+#else /* ifdef _WIN32 */
+# include <unistd.h>
+#endif /* ifdef _WIN32 */
 
 #define SLEEP_US 20000
 
@@ -12,7 +16,11 @@ int main(void)
     progressbar *smooth = progressbar_new("Smooth", max);
 
     for (int i = 0; i < max; i++) {
+#ifdef _WIN32
+        Sleep(SLEEP_US);
+#else /* ifdef _WIN32 */
         usleep(SLEEP_US);
+#endif /* ifdef _WIN32 */
         progressbar_inc(smooth);
     }
     progressbar_finish(smooth);
@@ -23,14 +31,22 @@ int main(void)
 
     for (int i = 0; i < 3; i++) {
         progressbar_inc(longlabel);
+#ifdef _WIN32
+        Sleep(1000);
+#else /* ifdef _WIN32 */
         sleep(1);
+#endif /* ifdef _WIN32 */
     }
     progressbar_finish(longlabel);
 
     progressbar *fast = progressbar_new("Fast", 20);
 
     for (int i = 0; i < 20; i++) {
+#ifdef _WIN32
+        Sleep(SLEEP_US / 1000);
+#else /* ifdef _WIN32 */
         usleep(SLEEP_US);
+#endif /* ifdef _WIN32 */
         progressbar_inc(fast);
     }
     progressbar_finish(fast);
@@ -38,7 +54,11 @@ int main(void)
     progressbar *custom = progressbar_new_with_format("Custom", max, "<.>");
 
     for (int i = 0; i < max; i++) {
+#ifdef _WIN32
+        Sleep(SLEEP_US / 1000);
+#else /* ifdef _WIN32 */
         usleep(SLEEP_US);
+#endif /* ifdef _WIN32 */
         progressbar_inc(custom);
     }
     progressbar_finish(custom);
